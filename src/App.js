7e1a3905e646d53124b71fdde49fc4ed693df157
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-22 19:12:48 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+      <div className="btn btn-primary">Search</div>
+    </div>
+  );
+}
+
+export default App;

80c641f92a120bb85e2e5ae65ef8e44c51298105
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-22 19:49:33 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
-      <div className="btn btn-primary">Search</div>
+      <div className="container">
+        <h1>Dictionary</h1>
+        <div className="btn btn-primary">Search</div>
+      </div>
+      <footer className="text-center mt-4">
+        <small>
+          {" "}
+          This project was coded by Olga Mang and is open-sourced on{" "}
+          <a
+            href="https://github.com/Ligeia1/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://clever-meninsky-674469.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify{" "}
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }

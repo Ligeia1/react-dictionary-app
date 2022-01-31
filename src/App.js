e034015db11d0c6bb9912fc75e9f729227b84354
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-31 17:01:53 +0100

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import background from "./image/bib.jpg";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Dictionary</h1>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "100%",
+        }}
+      >
+        <div className="container">
+          <h1>Dictionary</h1>
 
-        <Dictionary />
+          <Dictionary />
+        </div>
+        <footer className="text-center mt-4">
+          <small>
+            {" "}
+            This project was coded by Olga Mang and is open-sourced on{" "}
+            <a
+              href="https://github.com/Ligeia1/weather-app-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://clever-meninsky-674469.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify{" "}
+            </a>
+          </small>
+        </footer>
       </div>
-      <footer className="text-center mt-4">
-        <small>
-          {" "}
-          This project was coded by Olga Mang and is open-sourced on{" "}
-          <a
-            href="https://github.com/Ligeia1/weather-app-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://clever-meninsky-674469.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify{" "}
-          </a>
-        </small>
-      </footer>
     </div>
   );
 }

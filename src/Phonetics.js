854f11b5addb4832270fef76d9512cfee636a4ec
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-02-01 19:29:40 +0100

    Added Audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
+  const audio = new Audio(props.phonetics.audio);
+  function playSound() {
+    audio.play();
+  }
+
   return (
     <div className="Phonetics">
       <span className="phonetics-text">/{props.phonetics.text}/ </span>
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        <i className="fas fa-headphones"></i>
-      </a>
+      <i className="fas fa-headphones" onClick={playSound}></i>
     </div>
   );
 }
